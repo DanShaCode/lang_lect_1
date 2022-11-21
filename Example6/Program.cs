c27f94e76e44b9e7f4a692273a2350e38206d64e
Danis <danshadance@mail.ru>
2022-11-21 23:13:10 +0300

    Example6(Masha)

---
+++ Example6/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Enter your Name: ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "masha")
+{
+    Console.WriteLine("Hey! Its Maria!");
+}
+else
+{
+    Console.Write("Hello! ");
+    Console.WriteLine(username);
+}
